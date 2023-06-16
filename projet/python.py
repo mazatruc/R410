adcc91b05cec6e1cafdc46fc805d3e72b92e3808
Delacourt <mdelacourt2003@gmail.com>
2023-06-16 11:04:49 +0200

    projet 2eme

---
+++ projet/python.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import mysql
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'foo'
+app.config['MYSQL_DB'] = 'flask'
+
+mysql = MySQL(app)
+
+@app.route('/', methods = ['POST', 'GET'])
+def home():
+	if request.method == 'GET':
+		return "cheh"
+
+	if request.method == 'POST':
+
--- run_mysql.sh
+++ run_mysql.sh
@@ -1,5 +1,5 @@
 docker run --rm -d \
 	-p 3306:3306 \
-	--env MYSQL\_ROOT\_PASSWORD=foo \
+	--env MYSQL_ROOT_PASSWORD=foo \
 	-v vol-sql:/var/lib/mysql \
 	mysql
